92908be0dbfc3654a85ce1e9bcedcfd170457718
Deyvid Jimenez <82484153+ProdigySalazar@users.noreply.github.com>
2025-05-27 10:12:04 -0500

    add firebase and new model

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:recordatoriosapp/screens/homenotes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -10,8 +16,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomeNotes()
-    );
+    return const MaterialApp(home: MyHomeNotes());
   }
 }
---
+++ lib/models/Recordatorio.dart
@@ -0,0 +1,50 @@
+class Recordatorio {
+  int? id; // ID local (SQLite)
+  String titulo;
+  String descripcion;
+  DateTime fechaHora;
+  bool notificado; // Indica si ya se mostró la notificación
+  bool sincronizado; // Indica si ya fue sincronizado con el servidor
+
+  Recordatorio({
+    this.id,
+    required this.titulo,
+    required this.descripcion,
+    required this.fechaHora,
+    this.notificado = false,
+    this.sincronizado = false,
+  });
+
+  // Conversión a Map para SQLite
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'titulo': titulo,
+      'descripcion': descripcion,
+      'fechaHora': fechaHora.toIso8601String(),
+      'notificado': notificado ? 1 : 0,
+      'sincronizado': sincronizado ? 1 : 0,
+    };
+  }
+
+  // Crear objeto desde Map (SQLite)
+  factory Recordatorio.fromMap(Map<String, dynamic> map) {
+    return Recordatorio(
+      id: map['id'],
+      titulo: map['titulo'],
+      descripcion: map['descripcion'],
+      fechaHora: DateTime.parse(map['fechaHora']),
+      notificado: map['notificado'] == 1,
+      sincronizado: map['sincronizado'] == 1,
+    );
+  }
+
+  // Conversión a JSON (para enviar al servidor)
+  Map<String, dynamic> toJson() {
+    return {
+      'titulo': titulo,
+      'descripcion': descripcion,
+      'fechaHora': fechaHora.toIso8601String(),
+    };
+  }
+}
