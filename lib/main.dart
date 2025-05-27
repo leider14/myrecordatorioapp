d590f1e70792d38d14f7eae384f5765a369f13eb
Deyvid Jimenez <82484153+ProdigySalazar@users.noreply.github.com>
2025-05-27 10:26:00 -0500

    edit recordatorios

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:recordatoriosapp/screens/homenotes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
